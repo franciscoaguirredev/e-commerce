e9a3d641eb7adeb7421806ca03ba31848f83b03a
franciscoaguirredev <franco.hopper.riwi@gmail.com>
2024-08-22 14:45:54 -0500

    Other dependences Installed

---
+++ src/modules/users/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn('increment')
+    id:number
+
+    @Column()
+    email:string
+
+    @Column()
+    password:string
+
+    @Column()
+    roleId: number
+}
